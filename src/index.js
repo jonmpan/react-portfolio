b37cbf3df43f2b43694a9a8e1ec7f30d8e0b3488
Jonathan Pan <git config --global user.emailgit config --global user.emailgit config --global user.email>
2019-03-21 00:37:32 -0700

    fixed router with api

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 // api router
 app.use('/pokemon', pokemon({ config }));
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.server.listen(process.env.PORT || config.port, () => {
